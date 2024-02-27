c8a242e5fe66142cc0d1a8c4f48e0605f4350730
Eric Ardiansa <ericardiansa@gmail.com>
2024-02-27 15:43:40 +0700

    adding a few function

--- git_trojan.py
+++ git_trojan.py
@@ -19,3 +19,20 @@ def github_connect():
 #function to get file contents from repositories
 def get_file_contents(dirname,module_name, repo):
     return repo.file_contents(f'{dirname}/{module_name}').content
+
+#create trojan class to performs trojaning task
+class Trojan:
+    def __init__(self,id):
+        pass
+
+    def get_config(self):
+        pass
+
+    def module_runner(self, module):
+        pass
+
+    def store_module(self, data):
+        pass
+    
+    def run(self):
+        pass
